ca50cf506a543f8d205069bca0667bb1321db38e
Bunyawat Srisompong <bunyawat@blockfint.com>
2025-03-24 20:56:38 +0700

    Adds city search component and externalizes template
    Introduces a new city search feature allowing users to input city
    names or postal codes. Externalizes the main component's template
    to improve maintainability and structure by moving HTML to a
    separate file. Enhances the application with a more modular
    architecture by incorporating the CitySearchComponent.
    
    Facilitates easier future updates and improves code readability.

---
+++ projects/stage5/src/app/city-search/city-search.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core'
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+
+@Component({
+  selector: 'app-city-search',
+  templateUrl: './city-search.component.html',
+  styleUrl: './city-search.component.css',
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule],
+})
+export class CitySearchComponent {
+  search = new FormControl()
+}
