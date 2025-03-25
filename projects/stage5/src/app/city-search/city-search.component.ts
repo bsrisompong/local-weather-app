2e630ff11199e855f7413bfb955b34dcf970eccd
Bunyawat Srisompong <bunyawat@blockfint.com>
2025-03-25 13:39:05 +0700

    Enhances city search with weather service integration
    Introduces value change handling with RxJS operators, including
    debouncing, filtering, and side effects using tap, to process user
    input efficiently. Connects city search input to weather service
    for updating current weather conditions based on user query.
    
    Improves user experience by reducing unnecessary requests and
    ensuring valid search input before querying the weather API.

--- projects/stage5/src/app/city-search/city-search.component.ts
+++ projects/stage5/src/app/city-search/city-search.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core'
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
+import { debounceTime, filter, tap } from 'rxjs/operators'
+
+import { WeatherService } from '../weather/weather.service'
 
 @Component({
   selector: 'app-city-search',
@@ -12,6 +16,26 @@ import { MatInputModule } from '@angular/material/input'
 })
 export class CitySearchComponent {
   search = new FormControl('', [Validators.minLength(2)])
+
+  constructor(private weatherService: WeatherService) {
+    this.search.valueChanges
+      .pipe(
+        takeUntilDestroyed(),
+        filter((value) => value !== null && this.search.valid),
+        filter((value): value is string => value !== null),
+        debounceTime(1000),
+        tap((searchValue: string) => this.doSearch(searchValue)),
+        takeUntilDestroyed()
+      )
+      .subscribe()
+  }
+
+  doSearch(searchValue: string) {
+    const userInput = searchValue.split(',').map((s) => s.trim())
+    const searchText = userInput[0]
+    const country = userInput.length > 1 ? userInput[1] : undefined
+    this.weatherService.updateCurrentWeather(searchText, country)
+  }
 
   getErrorMessage() {
     return this.search.hasError('minLength')
